03634d8cfa56ea22d39c91a69e496f7e8d4464c9
ApazaMamaniWilsonR <171001ronald@gmail.com>
2025-08-22 15:29:10 -0500

    hola

---
+++ Asistencia/src/main/java/pe/edu/epeu/asistencia/Aplicacion.java
@@ -0,0 +1,8 @@
+package pe.edu.epeu.asistencia;
+
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("error 404");
+        AsistenciaApplication.main(args);
+    }
+}
---
+++ Asistencia/src/main/java/pe/edu/epeu/asistencia/control/AsistenciaController.java
@@ -0,0 +1,7 @@
+package pe.edu.epeu.asistencia.control;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AsistenciaController {
+}
