4dbd4fd5ac3e0ef7908d3c92ad4fb4187a2878a5
ApazaMamaniWilsonR <171001ronald@gmail.com>
2025-08-22 15:40:34 -0500

    delet

--- Asistencia/src/main/java/pe/edu/epeu/asistencia/AsistenciaApplication.java
+++ Asistencia/src/main/java/pe/edu/epeu/asistencia/AsistenciaApplication.java
@@ -1,10 +1,11 @@
 package pe.edu.epeu.asistencia;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AsistenciaApplication {
+public class AsistenciaApplication extends Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AsistenciaApplication.class, args);
