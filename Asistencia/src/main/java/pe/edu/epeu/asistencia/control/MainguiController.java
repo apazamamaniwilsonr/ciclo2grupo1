bf27c5e7242edb510a1b5c6aa08d22c0530746da
ApazaMamaniWilsonR <171001ronald@gmail.com>
2025-09-01 12:25:47 -0500

    main asistencia

---
+++ Asistencia/src/main/java/pe/edu/epeu/asistencia/control/MainguiController.java
@@ -0,0 +1,48 @@
+package pe.edu.epeu.asistencia.control;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import org.springframework.stereotype.Controller;
+
+import javax.swing.*;
+
+@Controller
+public class MainguiController {
+    @FXML
+    private BorderPane bp;
+    @FXML
+    private MenuBar menuBar;
+    @FXML
+    private TabPane tabPane;
+    @FXML
+    private Menu menu1;
+    @FXML
+    private MenuItem menuItem1;
+
+    public void initialize(){
+        MenuListener mL = new MenuListener();
+        MenuItemListener mIL = new MenuItemListener();
+        menuItem1.setOnAction(mIL::handle);
+    }
+
+    class MenuItemListener{
+        public void handle(ActionEvent e){
+
+            if(e.getSource()==menuItem1){
+                System.out.println(menuItem1.getText());
+            }
+        }
+    }
+
+    class MenuListener{
+        public void menuSelected(Event e){
+
+        }
+    }
+}
