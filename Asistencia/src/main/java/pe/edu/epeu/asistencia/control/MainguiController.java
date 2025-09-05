dd2fb28fe584efe9d10385da64a29725822fdf99
ApazaMamaniWilsonR <171001ronald@gmail.com>
2025-09-05 14:48:54 -0500

    main participante

--- Asistencia/src/main/java/pe/edu/epeu/asistencia/control/MainguiController.java
+++ Asistencia/src/main/java/pe/edu/epeu/asistencia/control/MainguiController.java
@@ -1,16 +1,19 @@
 package pe.edu.epeu.asistencia.control;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TabPane;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 
 import javax.swing.*;
+import java.util.Map;
 
 @Controller
 public class MainguiController {
@@ -23,21 +26,61 @@ public class MainguiController {
     @FXML
     private Menu menu1;
     @FXML
-    private MenuItem menuItem1;
+    private MenuItem menuItem1, menuItem2, menuItemC;
+    @Autowired
+    private ApplicationContext context;
 
     public void initialize(){
         MenuListener mL = new MenuListener();
         MenuItemListener mIL = new MenuItemListener();
         menuItem1.setOnAction(mIL::handle);
+        menuItem2.setOnAction(mIL::handle);
+        menuItemC.setOnAction(mIL::handle);
     }
 
     class MenuItemListener{
-        public void handle(ActionEvent e){
 
-            if(e.getSource()==menuItem1){
-                System.out.println(menuItem1.getText());
+        Map<String, String[]> menuconfig= Map.of(
+                "menuItem1", new String[]{"/fxml/main_asistencia.fxml","Gestion Asistencia","T"},
+                "menuItem2", new String[]{"/fxml/main_participante.fxml","Gestion Participante","T"},
+                "menuItemC", new String[]{"/fxml/login.fxml","Salir","C"}
+
+        );
+
+        public void handle(ActionEvent e){
+            String id=((MenuItem)e.getSource()).getId();
+            if(menuconfig.containsKey(id)){
+                String[] items=menuconfig.get(id);
+                if(items[2].equals("C")){
+                    Platform.exit();
+                    System.exit(0);
+                }else {
+                    abrirArchivofxml(items[0],items[1]);
+                }
             }
         }
+
+        public void abrirArchivofxml(String rutaARchivo, String titulo){
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(rutaARchivo));
+                fxmlLoader.setControllerFactory(context::getBean);
+                Parent root = fxmlLoader.load();
+
+                ScrollPane scrollPane = new ScrollPane(root);
+                scrollPane.setFitToWidth(true);
+                scrollPane.setFitToHeight(true);
+
+                Tab newtab = new Tab(titulo, scrollPane);
+                tabPane.getTabs().clear();
+                tabPane.getTabs().add(newtab);
+
+            }catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
+
+        }
+
     }
 
     class MenuListener{
---
+++ Asistencia/src/main/java/pe/edu/epeu/asistencia/control/ParticipanteController.java
@@ -0,0 +1,8 @@
+package pe.edu.epeu.asistencia.control;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ParticipanteController {
+
+}
